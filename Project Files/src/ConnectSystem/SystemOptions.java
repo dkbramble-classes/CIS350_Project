2895478cdba65114cf72047d320dfce70d8d3eed
Dane Bramble <Dane@DaneMAC.local>
2018-10-02 20:06:27 -0400

    updated SystemOptions package

--- Project Files/src/ConnectSystem/SystemOptions.java
+++ Project Files/src/ConnectSystem/SystemOptions.java
@@ -4,6 +4,8 @@ These are system functions to use during or before a game
 @author Dane Bramble
 @version September 2018
 *****************************************************************/
+package ConnectSystem;
+
 public class SystemOptions {
 	public static void ReturntoMenu(){
 		//I'm not sure what I should call.... I suppose it is whatever the menu is called
