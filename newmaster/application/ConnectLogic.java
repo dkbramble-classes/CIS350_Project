b850841877a98011a006789edebaee16721921aa
Dane Bramble <Dane@DaneMAC.local>
2018-10-19 01:20:34 -0400

    Added UML Class Diagram

---
+++ newmaster/application/ConnectLogic.java
@@ -0,0 +1,148 @@
+package gamelogic;
+
+public class ConnectLogic {
+
+  private int[][] gameGrid;
+  private int width;
+  private int height;
+  private int numPlayers;
+  private int activePlayer;
+
+  /**************************************************************************
+   * Connect Logic Constructor.
+   **************************************************************************/
+  public ConnectLogic(int width, int height, int numPlayers) {
+    gameGrid = new int[width][height];
+    this.width = width;
+    this.height = height;
+    this.numPlayers = numPlayers;
+    startGame(width, height);
+  }
+
+  /**************************************************************************
+   * Start Game: Initialize all spaces to be empty, and start with player 1.
+   **************************************************************************/
+  public void startGame(int width, int height) {
+    for (int i = 0; i < width - 1; i++) {
+      for (int j = 0; j < height - 1; j++) {
+        gameGrid[i][j] = 0;
+      }
+    }
+
+    activePlayer = 0;
+
+  }
+
+  /**************************************************************************
+   * Drop: Find the lowest space in the column, set it to be active player.
+   **************************************************************************/
+  public int drop(int col) {
+
+    int depth = 0;
+
+    // find the lowest point before chip hits bottom or another chip
+    while (gameGrid[col][depth + 1] == 0) {
+      depth++;
+      if (depth + 1 >= height) {
+        break;
+      }
+    }
+
+    // System.out.println(depth);
+    gameGrid[col][depth] = activePlayer;
+    return depth;
+  }
+
+  /**************************************************************************
+   * Next Turn: Increment whose turn it is.
+   **************************************************************************/
+  public void nextTurn() {
+    if (activePlayer < numPlayers) {
+      activePlayer++;
+    } else {
+      activePlayer = 1;
+    }
+    System.out.println("Player " + activePlayer + "'s turn");
+  }
+
+  /**************************************************************************
+   * Check Win: find if a line of 4 exists horizontally, vertically, or
+   * diagonally.
+   **************************************************************************/
+
+  public int checkWin() {
+    for (int i = 0; i < width; i++) {
+      for (int j = 0; j < height; j++) {
+
+        // check for each player
+        for (int p = 1; p <= numPlayers; p++) {
+
+          if (j < height - 3) {
+
+            // vertical win
+            if (gameGrid[i][j] == p && gameGrid[i][j + 1] == p && gameGrid[i][j + 2] == p 
+                && gameGrid[i][j + 3] == p) {
+              return p;
+            }
+          }
+
+          if (i < width - 3) {
+            // horizontal win
+            if (gameGrid[i][j] == p && gameGrid[i + 1][j] == p && gameGrid[i + 2][j] == p 
+                && gameGrid[i + 3][j] == p) {
+              return p;
+            }
+          }
+
+          if (i < width - 3 && j < height - 3) {
+
+            // diagonal down
+            if (gameGrid[i][j] == p && gameGrid[i + 1][j + 1] == p && gameGrid[i + 2][j + 2] == p
+                && gameGrid[i + 3][j + 3] == p) {
+              return p;
+            }
+          }
+
+          if (i < width - 3 && j > 3) {
+
+            // diagonal up
+            if (gameGrid[i][j] == p && gameGrid[i + 1][j - 1] == p && gameGrid[i + 2][j - 2] == p
+                && gameGrid[i + 3][j - 3] == p) {
+              return p;
+            }
+          }
+        }
+      }
+    }
+
+    return 0;
+  }
+
+  /**************************************************************************
+   * Is Valid: Returns the validity of a move, false if out of bounds.
+   **************************************************************************/
+  public boolean isValid(int col) {
+    return (col >= 0 && col <= width);
+  }
+
+  /**************************************************************************
+   * Get Grid.
+   **************************************************************************/
+  public int[][] getGrid() {
+    return gameGrid;
+  }
+
+  /**************************************************************************
+   * Get Y.
+   **************************************************************************/
+  public int getY() {
+    return height;
+  }
+
+  /**************************************************************************
+   * Get X.
+   **************************************************************************/
+  public int getX() {
+    return width;
+  }
+}
---
+++ newmaster/application/ConnectTest.java
@@ -0,0 +1,59 @@
+package gamelogic;
+import java.util.*;
+
+public class ConnectTest{
+	private static ConnectLogic logic;
+	
+	private static int[][] grid;
+
+	public static void main(String[] args){
+	
+		
+		Scanner scn = new Scanner(System.in);
+		System.out.print("--------\nWidth: ");
+		int width = scn.nextInt();
+		System.out.print("Height: ");
+		int height = scn.nextInt();
+		System.out.print("Number of players: ");
+		int players = scn.nextInt();
+		
+		logic = new ConnectLogic(width,height,players);
+		
+		
+		
+		grid = logic.getGrid();
+		
+		printBoard();
+	
+		int column = 0;	
+		do{
+			if(logic.checkWin() != 0){
+				System.out.println("Win!");
+				break;
+			}
+			logic.nextTurn();
+			column = scn.nextInt() - 1;
+			if(!logic.isValid(column)){
+				System.out.println("*out of bounds*");
+			}else{
+				logic.drop(column);
+				printBoard();	
+			}
+		}while(column != -1);
+	}
+
+	public static void printBoard(){			
+		
+		System.out.println("--------------");
+		for(int i=0;i<logic.getY();i++){
+			for(int j=0;j<logic.getX();j++){
+				System.out.print(grid[j][i] + " ");
+			}
+			System.out.println();
+		}
+
+		System.out.println("--------------");
+
+	}
+
+}
