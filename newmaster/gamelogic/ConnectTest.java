6ed652f63e7ea00d4d0a47d04ca876cdea7e18da
Ben Townsend <townsenb@mail.gvsu.edu>
2018-10-19 14:01:18 -0400

    Even More Documentation

--- newmaster/gamelogic/ConnectTest.java
+++ newmaster/gamelogic/ConnectTest.java
@@ -2,14 +2,26 @@ package gamelogic;
 
 import java.util.Scanner;
 
+
+/***********************************************************************
+  *Connect Test: Terminal interface for connect 4 game
+  *@author Ben Townsend
+  *@date 10/19/18
+  *********************************************************************/
+
 public class ConnectTest {
 	private static ConnectLogic logic;
 
 	private static int[][] grid;
 
-	public static void main(String[] args){
 	
-		
+    /******************************************************************
+      *Main method test
+      ****************************************************************/
+    
+    public static void main(String[] args){
+	
+		//setup board
 		Scanner scn = new Scanner(System.in);
 		System.out.print("--------\nWidth: ");
 		int width = scn.nextInt();
@@ -26,23 +38,34 @@ public class ConnectTest {
 		
 		printBoard();
 	
-		int column = 0;	
+		
+        //game loop
+        int column = 0;	
 		do{
+            //make sure no one has won yet
 			if(logic.checkWin() != 0){
 				System.out.println("Win!");
 				break;
 			}
+
+            //player input
 			logic.nextTurn();
 			column = scn.nextInt() - 1;
 			if(!logic.isValid(column)){
 				System.out.println("*out of bounds*");
-			}else{
+			
+            //drop the chip
+            }else{
 				logic.drop(column);
 				printBoard();	
 			}
 		}while(column != -1);
 	}
 
+
+    /***************************************************************
+      *Print Board: displays the current game state to terminal
+      *************************************************************/
 	public static void printBoard(){			
 		
 		System.out.println("--------------");
