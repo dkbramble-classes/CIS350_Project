65ccb61c9c50e8f96f27474bedac6b560e58ffb6
Patrik Kozak <patrikkozak@Patriks-MacBook-Pro.local>
2018-12-02 15:06:07 -0500

    Added functionality for game board

--- newmaster/View/GameManager.java
+++ newmaster/View/GameManager.java
@@ -1,13 +1,18 @@
 package View;
 
 
+import java.awt.EventQueue;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+import javax.swing.JOptionPane;
 import gamelogic.ConnectLogic;
 import javafx.animation.TranslateTransition;
+import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.effect.Light;
 import javafx.scene.effect.Lighting;
 import javafx.scene.image.Image;
@@ -23,38 +28,65 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import playertype.Player;
 
 
-public class GameManager{
+public class GameManager extends Application{
 
   private static final int TILE_SIZE = 80;
   private static final int COLUMNS = 7;
   private static final int ROWS = 6;
-  private AnchorPane gamePane;
+  private Pane gamePane;
   private Scene gameScene;
   private Stage gameStage;
+  private Shape gridShape;
   private Stage playerDetailsStage;
-  private boolean redMove = true; // new
-  private ConnectLogic game; // new
-  private Disc[][] grid =  new Disc[COLUMNS][ROWS]; // new
-  private Pane discRoot =  new Pane(); // new
-
+  private Disc[][] grid =  new Disc[COLUMNS][ROWS]; 
+  private Pane discRoot =  new Pane(); 
+  private ConnectLogic logic = new ConnectLogic(COLUMNS, ROWS); 
+  private String winner = "";
+  
   /******************************************************************
-  * Creates the game panel.
+  * Creates the game panel for when you start a new game from
+  * the Players Detail Manager;
   ******************************************************************/
   public GameManager() {
-    gamePane = new AnchorPane();
-    gameScene = new Scene(gamePane, 800, 700);
-    gameStage = new Stage();
-    gameStage.setScene(gameScene);
-    Shape gridShape = makeGrid();
-    gridShape.setLayoutX(100);
-    gridShape.setLayoutX(0); // changed
-    gamePane.getChildren().add(gridShape);
-    gamePane.getChildren().addAll(makeColumns());
-    gamePane.getChildren().add(discRoot); // new 
-    createBackground();
+
+	  logic.addPlayer(play2);
+	  logic.addPlayer(play4);
+	//logic.addPlayer(play1);
+	//logic.addPlayer(play3);
+	
+	  Pane gamePane = new Pane();
+      gameScene = new Scene(gamePane, 800, 700);
+      gameStage = new Stage();
+      gameStage.setScene(gameScene);
+      gridShape = makeGrid();
+  	  gamePane.getChildren().add(discRoot); 
+      gamePane.getChildren().add(gridShape);
+  	  gamePane.getChildren().addAll(makeColumns());
+  	  gridShape.setLayoutX(100);
+      gridShape.setLayoutX(0);
+    
   }
+  
+  /******************************************************************
+  * This method returns gamePane, which acts as the rectangles
+  * that allow you to pick what column to place your chip. 
+  * Without this, the option to place another 
+  ******************************************************************/
+  private Parent createContent(){
+  	
+    createBackground();
+    return gamePane;
+  }
+  
+  Player play2 = new Player("r", 1, "Mr. Frodo");
+  Player play4 = new Player("g", 2, "One-Eyed Willy");
+  
+//Player play1 = new Player("d", 1, "hackerman", "m", true);
+//Player play3 = new Player("d", 3, "DESTROYEROFWORLDS", "m", true);
+
   
   /******************************************************************
   * Goes to the next page in the game menu.
@@ -67,9 +99,16 @@ public class GameManager{
   }
   
   /******************************************************************
+   * Setter that takes in logic from ConnectLogic.
+   ******************************************************************/
+  public void setLogic(ConnectLogic logic) {
+	  this.logic = logic;
+  }
+  
+  /******************************************************************
   * Creates the game board grid.
   ******************************************************************/
-  private Shape makeGrid() {
+  private Shape makeGrid(){
     Shape shape = new Rectangle((COLUMNS + 1) * TILE_SIZE, 
         (ROWS + 1) * TILE_SIZE);
     for(int y = 0; y < ROWS; y++) {
@@ -79,10 +118,10 @@ public class GameManager{
         circle.setCenterY(TILE_SIZE / 2);
         circle.setTranslateX(x * (TILE_SIZE + 5) + TILE_SIZE / 3);
         circle.setTranslateY(y * (TILE_SIZE + 5) + TILE_SIZE / 3);
+        
         shape = Shape.subtract(shape, circle);
-
       }
-    }
+    } 
     
     Light.Distant light = new Light.Distant();
     light.setAzimuth(45.0);
@@ -104,26 +143,22 @@ public class GameManager{
   * choosing to drop a chip into.
   ******************************************************************/
   private List<Rectangle> makeColumns(){
-    List<Rectangle> list = new  ArrayList<>();
-
-    for(int x = 0; x < COLUMNS; x++) {
-      Rectangle rect = new Rectangle(TILE_SIZE , (ROWS + 1)
-          * TILE_SIZE);
-      rect.setTranslateX(x * (TILE_SIZE + 5) + TILE_SIZE / 3);
-      rect.setFill(Color.TRANSPARENT);
-
-            rect.setOnMouseEntered(e -> rect.setFill
-                (Color.rgb(200, 200, 50, 0.3)));
-            rect.setOnMouseExited(e -> rect.setFill
-                (Color.TRANSPARENT));
-            
-            final int column = x;
-            rect.setOnMouseClicked(e -> placeDisc(new Disc(redMove), column));
-
-            list.add(rect);
-
+      List<Rectangle> list = new ArrayList<>();
+     
+      for(int x = 0; x < COLUMNS; x++) {
+          Rectangle rect = new Rectangle(TILE_SIZE, (ROWS + 1) * TILE_SIZE);
+          rect.setTranslateX(x * (TILE_SIZE + 5) + TILE_SIZE / 3);
+          rect.setFill(Color.TRANSPARENT);
+         
+          rect.setOnMouseEntered(e -> rect.setFill(Color.rgb(200, 200, 50, 0.3)));
+          rect.setOnMouseExited(e -> rect.setFill(Color.TRANSPARENT));
+         
+          final int column = x;
+          rect.setOnMouseClicked(e -> placeDisc(new Disc(findColor()), column));
+        
+          list.add(rect);
         }
-        return list;
+    	return list;
     }
   
   /******************************************************************
@@ -131,36 +166,48 @@ public class GameManager{
   ******************************************************************/
   private void placeDisc(Disc disc, int column) {
 	  int row = ROWS - 1;
-	  do{
-		  if(!getDisc(column, row).isPresent())
-			  break;
+	  if(!logic.getCurrentPlayer().getCompStatus()) {
+		  do{
+			  if(!getDisc(column, row).isPresent())
+				  break;
+			  
+			  row--;
+		  } while(row >= 0);
+		  if(row < 0)
+			  return;
 		  
-		  row--;
-	  } while(row >= 0);
-	  if(row < 0)
-		  return;
-	  
-	  grid[column][row] = disc;
-	  discRoot.getChildren().add(disc);
-	  disc.setTranslateX(column * (TILE_SIZE + 5) + TILE_SIZE / 3);
-	  
-	  TranslateTransition animation = new TranslateTransition(Duration.seconds(0.5), disc);
-	  animation.setToY(row * (TILE_SIZE + 5) + TILE_SIZE / 3);
-	  animation.setOnFinished(e -> {
-		  if(game.checkWin() == 1) {
-			  gameOver();
+		  grid[column][row] = disc;
+		  discRoot.getChildren().add(disc);
+		  disc.setTranslateX(column * (TILE_SIZE + 5) 
+				  + (TILE_SIZE / 3));
+		  
+		  TranslateTransition animation = new TranslateTransition
+				  (Duration.seconds(0.5), disc);
+		  animation.setToY(row * (TILE_SIZE + 5) + (TILE_SIZE / 3));
+		  
+		  logic.nextTurn(column);
+		  if(logic.checkWin() == 1){ //if won
+                ShowMessage(winner + " Wins!");
+                  
 		  }
-		  redMove = !redMove;
-	  }); 
-	  animation.play(); 
+		  winner = logic.getCurrentPlayer().getName();
+		  animation.play();
+		  
+	  }
   }
   
-  private void gameOver() {
-	  System.out.println("Winner: " + (redMove ? "RED" : "Yellow"));
+  private void ShowMessage(String message) {
+	  Alert alert = new Alert(AlertType.INFORMATION);
+	  alert.setTitle("Game Finished");
+	  alert.setHeaderText(message);
+	  alert.setContentText("GAME IS OVER, RETURNING TO MENU.");
+	  
+	  alert.show();
   }
   
   private Optional<Disc> getDisc(int column, int row){
-	  if(column < 0 || column >= COLUMNS || row < 0 || row >= ROWS)
+	  if(column < 0 || column >= COLUMNS 
+			  || row < 0 || row >= ROWS)
 		  return Optional.empty();
 	  
 	  return Optional.ofNullable(grid[column][row]);
@@ -171,19 +218,16 @@ public class GameManager{
   * them as players. 
   ******************************************************************/
   private static class Disc extends Circle{
-	  private final boolean red;
-	  public Disc(boolean red) {
-		  super(TILE_SIZE /2, red ? Color.RED : Color.YELLOW);
-		  this.red = red;
+	  public Disc(Color playcolor) {
+		  super(TILE_SIZE /2, playcolor);
 		  
 		  setCenterX(TILE_SIZE / 2);
 		  setCenterY(TILE_SIZE / 2);
 	  }
   }
   
-private void createBackground() {
+  private void createBackground() {
   
-    
     Image backgroundImage = new Image("model/resources/"
         + "Slider-CL01-Background-256x256.png", 256,256,false,true);
     BackgroundImage background = new BackgroundImage(backgroundImage, 
@@ -193,5 +237,30 @@ private void createBackground() {
     
   }
 
+  private Color findColor(){
+	  Color color = Color.RED;
+	  String playcolor = logic.getCurrentPlayer().getColor();
+	  switch (playcolor) {
+	  	case "r":
+	  		color =  Color.RED;
+	  		break;
+	  	case "b":
+	  		color = Color.BLUE;
+	  		break;
+	  	case "g":
+	  		color = Color.GREEN;
+	  		break;
+	  	case "w":
+	  		color = Color.WHITE;
+	  		break;
+	  }
+	  return color;
+	}
+
+  @Override
+  public void start(Stage stage) throws Exception{ 
+	  stage.setScene(new Scene(createContent()));
+	  stage.show();
+	}
 
 }
